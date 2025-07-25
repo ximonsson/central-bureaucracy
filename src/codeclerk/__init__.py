99e6d40b9b9b7aa71c0ddb2a2e10c766e541c461
Simon Simonsson <simon.simonsson@posten.no>
2025-07-25 15:17:50 +0200

    Add Code Clerk
    Just squelleton code so it is there.

---
+++ src/codeclerk/__init__.py
@@ -0,0 +1,28 @@
+import agents
+import openai
+import os
+
+
+def agent(model: str, instr: str, temp: float = 0.0):
+    """
+    Creates an agent with the specified model, instructions, and temperature.
+
+    Args:
+        model (str): The model to use for the agent.
+        instr (str): The instructions for the agent.
+        temp (float, optional): The temperature for the model. Defaults to 0.0.
+
+    Returns:
+        agents.Agent: The created agent.
+    """
+
+    c = openai.AsyncOpenAI(base_url=os.environ["OPENAI_API_BASE"])
+    m = agents.OpenAIChatCompletionsModel(openai_client=c, model=model)
+
+    return agents.Agent(
+        model=m,
+        name="Ink Inspector 7000",
+        instructions=instr,
+        model_settings=agents.ModelSettings(temperature=temp),
+        tools=[],
+    )
