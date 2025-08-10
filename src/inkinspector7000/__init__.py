ed66535d74c04d5a16b4204682eae6d9070b06e3
Simon Simonsson <simonsson.simon@gmail.com>
2025-08-10 13:32:07 +0200

    Image model for transcription

--- src/inkinspector7000/__init__.py
+++ src/inkinspector7000/__init__.py
@@ -12,8 +12,20 @@ def encode(path: str) -> str:
 
 
 def ocr(path: str, model_id: str = "mistral-ocr-latest") -> dict:
+    """
+    Perform OCR on an image using the Mistral OCR model.
+
+    Args:
+        path (str): The path to the image file.
+        model_id (str, optional): The ID of the OCR model to use. Defaults to "mistral-ocr-latest".
+
+    Returns:
+        dict: The OCR result.
+    """
+
     img = encode(path)
-    client = Mistral(api_key=os.environ["MISTRAL_API_KEY"])
+    # client = Mistral(api_key=os.environ["MISTRAL_API_KEY"])
+    client = Mistral(server_url="http://localhost:4000/mistral/v1")
 
     res = client.ocr.process(
         model=model_id,
@@ -24,6 +36,38 @@ def ocr(path: str, model_id: str = "mistral-ocr-latest") -> dict:
         include_image_base64=True,
     )
 
+    return res
+
+
+def transcribe(model: str, img_path: str):
+    """
+    Transcribes an image to markdown using a specified model.
+
+    Args:
+        model (str): The model to use for transcription.
+        img_path (str): The path to the image to transcribe.
+
+    Returns:
+        The response from the transcription model.
+    """
+
+    c = openai.OpenAI(base_url=os.environ["OPENAI_API_BASE"])
+    img = encode(img_path)
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "Transcribe this to markdown"},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{img}",
+                    },
+                },
+            ],
+        }
+    ]
+    res = c.chat.completions.create(model=model, messages=messages)
     return res
 
 
